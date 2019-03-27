4566903d9c181e1ae03b3826a5a667986c3a0c12
Vanessa Latefa Pampilo <vanessalatefapampilo@Vanessas-MacBook-Pro.local>
2019-03-27 18:18:00 +0100

    finally fixed the HomeListComponent error

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeListComponent } from './home-list/home-list.component';
+import {MaterialModule} from '../material/material';
 
 @NgModule({
   declarations: [HomeListComponent], 
   imports: [
     CommonModule,
     HomeRoutingModule,
-    HomeListComponent,
+    MaterialModule
+
     
   ]
 })
